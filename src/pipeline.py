9fe2d2eebad7828cfe054267230710cb511ad6b9
robert <montey@meppelink.net>
2024-10-03 11:09:01 +0000

    new

--- src/pipeline.py
+++ src/pipeline.py
@@ -1,13 +1,18 @@
 import torch
 from PIL.Image import Image
-from diffusers import StableDiffusionXLPipeline,DPMSolverMultistepScheduler
+from diffusers import StableDiffusionXLPipeline
 from pipelines.models import TextToImageRequest
 from torch import Generator
-from tgate import TgateSDXLDeepCacheLoader
+from sfast.compilers.diffusion_pipeline_compiler import (compile,
+                                                         CompilationConfig)
 
-GATE_STEP = 10
-INF_STEP= 30
+import packaging.version
+import torch
 
+if packaging.version.parse(torch.__version__) >= packaging.version.parse('1.12.0'):
+    torch.backends.cuda.matmul.allow_tf32 = True
+
+INF_STEP= 18
 def load_pipeline() -> StableDiffusionXLPipeline:
     pipeline = StableDiffusionXLPipeline.from_pretrained(
         "./models/newdream-sdxl-21/",
@@ -15,19 +20,45 @@ def load_pipeline() -> StableDiffusionXLPipeline:
         use_safetensors=True, 
         local_files_only=True,
     )
+    pipeline.to("cuda")
+    config = CompilationConfig.Default()
 
-    pipeline.scheduler = DPMSolverMultistepScheduler.from_config(pipeline.scheduler.config)
+    try:
+        import triton
+        config.enable_triton = True
+    except ImportError:
+        print('Triton not installed, skip')
 
-    pipeline = TgateSDXLDeepCacheLoader(
-                   pipeline,
-                          cache_interval=3,
-                                 cache_branch_id=0,
-                                 ).to("cuda")
+    config.enable_cuda_graph = True
+    
+    
+    pipeline  = compile(pipeline, config)
+    
+    kwarg_inputs = dict(
+        prompt=
+        '(masterpiece:1,2), best quality, masterpiece, best detailed face, a beautiful girl',
+        height=512,
+        width=512,
+        num_inference_steps=INF_STEP,
+        num_images_per_prompt=1,
+    )
+
+    for _ in range(3):
+        output_image = pipeline(**kwarg_inputs).images[0]
     pipeline(prompt="")
     return pipeline
 
 def infer(request: TextToImageRequest, pipeline: StableDiffusionXLPipeline) -> Image:
     generator = Generator(pipeline.device).manual_seed(request.seed) if request.seed else None
+    return pipeline(
+        prompt=request.prompt,
+        negative_prompt=request.negative_prompt,
+        width=request.width,
+        height=request.height,
+        generator=generator,
+        num_inference_steps=INF_STEP,
+        num_images_per_prompt=1,
+    ).images[0] 
     return pipeline.tgate(
         prompt=request.prompt,
         negative_prompt=f"{request.negative_prompt}",
